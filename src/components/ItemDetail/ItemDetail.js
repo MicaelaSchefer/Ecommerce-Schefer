3ed685c0269fec56cb1eb230657692eb408aa7b9
MicaelaSchefer <95242060+MicaelaSchefer@users.noreply.github.com>
2022-08-02 20:32:04 -0300

    arreglos codigo detail

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -4,8 +4,8 @@ import ItemCount from './ItemCount';
 const ItemDetail = ({ product }) => {
     return(
         <div>
-             <h5>{product.name}</h5>
-               <p>{product.precio}</p>
+             <h5>{product.id}</h5>
+               <p>{product.price}</p>
                <ItemCount/>
         </div>
     );
